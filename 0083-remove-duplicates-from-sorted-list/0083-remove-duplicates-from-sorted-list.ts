cab486e75cd8411747c970ce3f63343a843ddad8
Rudy Haddad <90129980+rudy002@users.noreply.github.com>
2025-09-21 17:10:09 +0300

    Time: 0 ms (100%), Space: 58 MB (88.3%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let dummy = new ListNode();
+    if (head === null) return null;
+
+    let current: ListNode | null = head;
+
+    
+    while (current && current.next)
+    {
+        if (current.val === current.next.val)
+        {
+            current.next = current.next.next   
+        }
+        else
+        {
+            current = current.next
+        }
+
+    }
+    return head
+
+};
