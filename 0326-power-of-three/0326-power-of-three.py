32770a6bcb40460f3e55e386026681af156e6553
Rudy Haddad <90129980+rudy002@users.noreply.github.com>
2026-01-19 18:29:51 +0200

    Time: 0 ms (100%), Space: 19.4 MB (17.77%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,5 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
+        
+        # utiliser le plus grand nombre des int et le diviser par n (a test parce que pas sur)
         if n == 0:
             return False
         elif n < 0:
