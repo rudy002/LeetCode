957ed76c51172276ea7a0695ea69ac24d3500b69
Rudy Haddad <90129980+rudy002@users.noreply.github.com>
2026-06-02 16:30:46 +0300

    Time: 0 ms (100%), Space: 19.4 MB (40.24%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,34 +1,13 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-    
-        phone = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z']
-        }
+        buttons = {"2" : "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        groups = []
+        result = [""]
 
-        if digits == "":
-            return []
+        for d in digits: 
+            groups.append(buttons[d])
 
-        if len(digits) == 1:
-            return phone[digits[0]]
-
-        res = ['']
-
-        for digit in digits:
-            letter = phone[digit]
-            new_res = []
-
-            for x in res:
-                for y in letter:
-                    new_res.append(x + y)
-            
-            res = new_res
+        for group in groups:
+            result =  [x + y for x in result for y in group]
         
-        return res
-        
+        return result
